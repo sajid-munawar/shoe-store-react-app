377c903a2ea0b23996ed918c74f30a97814aab28
sajid-munawar <sajidmunawar666@gmail.com>
2020-11-13 07:21:25 +0500

    reach at product items

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,10 +1,23 @@
 import React from "react";
+import shoes from "../Shoes.json"
 
 
 function Product() {
+  console.log(shoes)
+  // console.log(Object.keys(shoes))
   return (
     <div >
      <h1> Welcome to product </h1>
+     <div className="shoes">
+       {Object.keys(shoes).map(keyName =>{
+         const shoe=shoes[keyName]
+         return (<div key={keyName}> 
+           <h4>{shoe.name}</h4>
+           <img src={shoe.img} alt="shoes" height={150}/>
+         </div>)
+       })}
+     </div>
+    
     </div>
   );
 }
